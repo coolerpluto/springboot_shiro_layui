fe19496cc6cdc1206198af681524ba3743728ebf
coolerpluto <8039035008@qq.com>
2022-04-21 22:46:31 +0800

    重写部门实现类方法

--- erp/src/main/java/com/example/sys/service/impl/DeptServiceImpl.java
+++ erp/src/main/java/com/example/sys/service/impl/DeptServiceImpl.java
@@ -6,13 +6,28 @@ import com.example.sys.service.DeptService;
 import com.example.sys.mapper.DeptMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 /**
  *
  */
 @Service
-public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept>
-    implements DeptService{
+public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept> implements DeptService{
 
+    @Override
+    public Dept getById(Serializable id) {
+        return super.getById(id);
+    }
+
+    @Override
+    public boolean updateById(Dept entity) {
+        return super.updateById(entity);
+    }
+
+    @Override
+    public boolean removeById(Serializable id) {
+        return super.removeById(id);
+    }
 }
 
 
